46cb5043700c7013dab5e7408a5dc2cc2ff0dfb5
MarcosAriel9 <20203tn043@utez.edu.mx>
2021-06-25 16:09:59 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+const mensaje = () => {
+    let busca = document.getElementById("busc").value
+    alert(`tu busqueda no ha sido concretada ya que: ${busca} no se encuentra en nuestras busquedas`)
+}
